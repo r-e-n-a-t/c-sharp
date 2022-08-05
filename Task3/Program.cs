6051357e067b60eca742f2445e0e7a90246799b9
r-e-n-a-t <ren2013ner@gmail.com>
2022-08-05 16:14:05 +1000

    init commit

---
+++ Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = int.Parse(value);
+    return result;
+}
+
+string ParityCheck(int num)
+{
+    string result;
+    if ((num % 2) == 0)
+    {
+        result = $"Число {num} чётное";
+    }
+    else
+    {
+        result = $"Число {num} нечётное";
+    }
+    return result;
+}
+
+int num = Prompt("Введите число >");
+
+System.Console.WriteLine(ParityCheck(num));
---
+++ Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = int.Parse(value);
+    return result;
+}
+
+int num = Prompt("Введите число >");
+int count = 1;
+while (count <= num) {
+    if ((count % 2) == 0)
+    {
+        System.Console.Write($"{count}, ");
+    }
+    count++;
+}
+
+// System.Console.WriteLine(ParityCheck(num));
