f306327dbb608895955c3d7425756ef79340798b
r-e-n-a-t <ren2013ner@gmail.com>
2022-08-10 22:12:51 +1000

    add 2 task

---
+++ lesson-2/Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+void randomArray (int[] array){
+    for (int i = 0; i < array.Length; i++) {
+        array[i] = new Random().Next(1, 10);
+    }
+}
+int[] array = new int[3];
+randomArray(array);
+System.Console.WriteLine($"{array[0]}{array[1]}{array[2]}");
+System.Console.WriteLine($"{array[0]}{array[2]}");
