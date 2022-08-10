77168439e9bd05473d41576fca1f5176053ddbe3
r-e-n-a-t <ren2013ner@gmail.com>
2022-08-10 22:40:52 +1000

    add 4 task

--- lesson-2/Task4/Program.cs
+++ lesson-2/Task4/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 4: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+string Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int num = int.Parse(value);
+    string result = "Такого дня недели нет :)";
+    if (num > 0 && num < 8)
+    {
+        result = "Рабочий день";
+
+        if (num == 6 || num == 7)
+        {
+            result = "Выходной";
+        }
+    }
+    return result;
+}
+
+string message = Prompt("Введите номер дня недели>");
+System.Console.Write(message);
+
+
+
