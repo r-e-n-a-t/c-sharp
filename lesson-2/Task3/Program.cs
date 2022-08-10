1390dff2734c67656f666c9900b870d73e47b34f
r-e-n-a-t <ren2013ner@gmail.com>
2022-08-10 22:25:19 +1000

    add 3 task

--- lesson-2/Task3/Program.cs
+++ lesson-2/Task3/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+string Prompt(string message)
+{
+    System.Console.Write(message);
+    string result = Console.ReadLine();
+    if (result.Length < 3)
+    {
+        result = "error";
+    }
+    return result;
+}
+
+string message = Prompt("Введите число >");
+if (message == "error")
+{
+    System.Console.Write("Третьей цифры нет");
+}
+else
+{
+    System.Console.Write(message[2]);
+}
+
+
