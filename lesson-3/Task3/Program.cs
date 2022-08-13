6b1447512e16c59a42c4aa104fb8934b9d48e9a8
r-e-n-a-t <ren2013ner@gmail.com>
2022-08-13 14:27:49 +1000

    add task 3 of leson 3

--- lesson-3/Task3/Program.cs
+++ lesson-3/Task3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = int.Parse(value);
+    return result;
+}
+void cubedNum(int num) {
+    for (int i = 1; i <= num; i++) {
+    System.Console.Write(i * i * i + "  ");
+    }
+}
+int message = Prompt("Введите число >");
+System.Console.WriteLine($"Таблица кубов чисел от 1 до {message}:");
+cubedNum(message);
