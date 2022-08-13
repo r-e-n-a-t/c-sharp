e907a0d2e19cea533d79ee427748b8f6e877f300
r-e-n-a-t <ren2013ner@gmail.com>
2022-08-13 13:35:44 +1000

    add task 1 of leson 3

---
+++ lesson-3/Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 1. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+string Prompt(string message)
+{
+    System.Console.Write(message);
+    string result = Console.ReadLine();
+    return result;
+}
+string NumberRev(string value)
+{
+    int num = int.Parse(value);
+    string result = "";
+    int a = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        a = num % 10;
+        num = num / 10;
+        result += a;
+    };
+    return result;
+}
+string NumberСheck(string value1, string value2)
+{
+    string result = $"Число {value1} не является палиндромом";
+    if (value1 == value2)
+    {
+        result = $"Число {value1} является палиндромом";
+    }
+    return result;
+}
+
+string numberOne = Prompt("Введите число >");
+string numberTwo = NumberRev(numberOne);
+string message = NumberСheck (numberOne, numberTwo);
+System.Console.Write(message);
