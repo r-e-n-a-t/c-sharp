c004a19d51eef1ddf133246f3cc2d2c83ecefb4d
r-e-n-a-t <ren2013ner@gmail.com>
2022-08-13 17:50:57 +1000

    add task 4 of leson 3

---
+++ lesson-3/Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] Sort(int[] array){
+	for(int i = 1; i < array.Length; i++){
+		for(int j = i; j > 0 && array[j-1] > array[j]; j--){
+			int tmp = array[j-1];
+			array[j-1] = array[j];
+			array[j] = tmp;
+		}
+	}
+    return array;
+}
+void ArrShow(int[] array) {
+    for (int i = 0; i < array.Length; i++) {
+        System.Console.Write(array[i]);
+    }
+}
+int[] array = new int[] {4, 3, 8, 0, 1, 7, 2, 5, 6, 9};
+Sort(array);
+ArrShow(array);
