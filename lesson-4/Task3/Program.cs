40ca6abcd2e04da300c3f2415b18a19b7146bb97
r-e-n-a-t <ren2013ner@gmail.com>
2022-08-17 20:10:50 +1000

    change code task 2

--- lesson-4/Task3/Program.cs
+++ lesson-4/Task3/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая задаёт массив из 8 элементов, выводит их на экран. И ищет второй максимум (элемент меньше максимального, но больше всех остальных)
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = int.Parse(value);
+    return result;
+}
+
+int Sum(int number)
+{   
+    int result = 0;
+    while (number > 0)
+    {
+        int a = number % 10;
+        result += a;
+        number = number / 10;
+    };
+    return result;
+}
+
+int number = Prompt("Введите число >");
+int sumNumber = Sum(number);
+System.Console.Write(sumNumber);
+
+
