6e131910c2e696a279fd4bd11473f0dea312a76a
r-e-n-a-t <ren2013ner@gmail.com>
2022-08-17 19:56:32 +1000

    add task 1 of lesson 4

---
+++ lesson-4/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = int.Parse(value);
+    return result;
+}
+
+double PowerNumber(int a, int b)
+{   
+    double result = 1;
+    for (int i = 0; i < b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+
+int a = Prompt("Введите первое число >");
+int b = Prompt("Введите второе число >");
+double number = PowerNumber(a, b);
+
+System.Console.Write(number);
+
+
