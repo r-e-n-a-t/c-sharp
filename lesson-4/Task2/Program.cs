679c3627b4317a1608dae0e6153099995365384b
r-e-n-a-t <ren2013ner@gmail.com>
2022-08-17 20:09:38 +1000

    add task 2 of lesson 4

--- lesson-4/Task2/Program.cs
+++ lesson-4/Task2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = int.Parse(value);
+    return result;
+}
+
+int Sum(int number)
+{   
+    int result = 0;
+    while (number > 0)
+    {
+        int a = number % 10;
+        result += a;
+        number = number / 10;
+    };
+    return result;
+}
+
+int number = Prompt("Введите число >");
+int sumNumber = Sum(number);
+System.Console.Write(sumNumber);
+
+
