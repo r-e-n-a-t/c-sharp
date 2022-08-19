4cd0956a73ddb6b0772f795b9cad251bfb95ffd1
r-e-n-a-t <ren2013ner@gmail.com>
2022-08-19 12:26:51 +1000

    add task 1 of lesson 5

---
+++ lesson-5/Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] CreateArray(int len, int min, int max)
+{
+    int[] array = new int[len];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+    return array;
+}
+
+void showArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}  ");
+    }
+    System.Console.WriteLine();
+}
+
+int FindPositiveNums(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            result++;
+        }
+    };
+    return result;
+}
+
+int[] array = CreateArray(4, 100, 1000);
+showArray(array);
+System.Console.WriteLine($"Количество чётных чисел в массиве => {FindPositiveNums(array)}");
+
+
