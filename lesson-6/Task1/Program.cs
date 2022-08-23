950634d676f06117566fd5a1a1be557f681faea3
r-e-n-a-t <ren2013ner@gmail.com>
2022-08-23 13:12:43 +1000

    change task 1 of lesson 6

---
+++ lesson-6/Task1/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+string Prompt(string message)
+{
+    System.Console.Write(message);
+    string result = Console.ReadLine();
+    // int result = int.Parse(value);
+    return result;
+}
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}  ");
+    }
+    System.Console.WriteLine();
+}
+int[] CreateArrNum()
+{   
+    int count = 0;
+    string number = "";
+    int[] tempArray = new int[1000];
+    System.Console.WriteLine($"Для выхода из ввода чисел введите 'exit'");
+    for (count = 0; number != "exit"; count++)
+    {
+        number = Prompt($"Введите число {count+1}>");
+        if (number != "exit") 
+        {
+            tempArray[count] = int.Parse(number);
+        }
+    }
+    int[] array = new int[count-1];
+    for (int i = 0; i < array.Length; i++)
+    {
+         array[i] = tempArray[i];
+    }
+    return array;
+}
+void FindMoreZeroNum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            count++;
+        }
+    }
+    System.Console.WriteLine($"Количество введённых чисел больше 0 => {count}");
+}
+int[] arr = CreateArrNum();
+ShowArray(arr);
+FindMoreZeroNum(arr);
+
+
+
