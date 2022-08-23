eba289e093a9eea20cde1be577c843ae7b758d53
r-e-n-a-t <ren2013ner@gmail.com>
2022-08-23 14:14:30 +1000

    change task 3 of lesson 6

--- lesson-6/Task3/Program.cs
+++ lesson-6/Task3/Program.cs
@@ -1,1 +1,29 @@
 ﻿// Задача 3. Со Звездочкой (*). Сгенерировать двумерный массив со случайными числами, вывести его на экран, оформить в виде функций (пригодится в дальнейшем)
+
+int[,] CreateTwoDimArray(int lines, int columns, int min, int max)
+{
+    int[,] array = new int[lines, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+        }
+    return array;
+}
+
+void showTwoDimArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}  ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] array = CreateTwoDimArray(2, 10, 10, 100);
+showTwoDimArray(array);
