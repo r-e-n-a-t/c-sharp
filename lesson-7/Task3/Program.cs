89b3d617f316e65575ac21db2798458175227bf4
r-e-n-a-t <ren2013ner@gmail.com>
2022-08-26 15:27:43 +1000

    add task 3 of lesson 7

--- lesson-7/Task3/Program.cs
+++ lesson-7/Task3/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = int.Parse(value);
+    return result;
+}
+
+int[,] CreateTwoDimArray(int lines, int columns, int min, int max)
+{
+    int[,] array = new int[lines, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+
+void showTwoDimArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void ShowColumnAverage(int[,] array) 
+{
+    double result = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            result += array[j, i];
+        }
+        System.Console.WriteLine($"Среднее арифм. элементов в {i+1} столбце = {Math.Round(result/array.GetLength(0), 1)}");
+        result = 0;
+    }
+}
+
+int[,] array = CreateTwoDimArray(Prompt("Введите количество строк > "), Prompt("Введите количество столбцов > "), 1, 10);
+showTwoDimArray(array);
+ShowColumnAverage(array);
+
+
