7f972b772b42dcd4288c82810fa4f823c4f9ba75
r-e-n-a-t <ren2013ner@gmail.com>
2022-09-04 13:30:56 +1000

    add task 2 of lesson 9

--- lesson-9/Task2/Program.cs
+++ lesson-9/Task2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+int sum = 0;
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = int.Parse(value);
+    return result;
+}
+
+void FindSum(int m, int n)
+{
+    if (m <= n) 
+    {
+        sum += m;
+        FindSum(m + 1, n);
+    }
+}
+
+FindSum(Prompt("Введите число M > "), Prompt("Введите число N > "));
+System.Console.WriteLine($"Cумма элементов в промежутке от M до N -> {sum}");
